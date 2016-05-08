d7c275768fb80f0c9b30725886178aab8b9a94e4
nfactorial <nfactorial@btopenworld.com>
2016-05-08 10:30:27 +0100

    Added light controller for card collection room.

--- development/scripts/card_collection/light_controller.js
+++ development/scripts/card_collection/light_controller.js
@@ -1,4 +1,5 @@
 pc.script.attribute('speed', 'number', 1);
+pc.script.attribute('phaseShift', 'number', 0);
 
 pc.script.create('light_controller', function (app) {
     // Creates a new Spotlight instance
@@ -10,19 +11,19 @@ pc.script.create('light_controller', function (app) {
 
     LightController.prototype = {
         // Called once after all resources are loaded and before the first update
-        initialize: function () {
+        initialize: function() {
             this.rotation.set(0,0,0);
-
-            this.rotation = 0;
             this.timer = 0.0;
         },
 
         // Called every frame, dt is time in seconds since last update
-        update: function (dt) {
+        update: function(dt) {
             this.timer += dt;
 
-            this.rotation.y += dt * 180;
-            this.entity.setLocalEulerAngles(this.rotation);
+            this.rotation.x = this.phaseShift + this.speed * 180 * this.timer;
+            this.rotation.y = 0;
+            this.rotation.z = 90;
+            this.entity.setEulerAngles(this.rotation.x, this.rotation.y, this.rotation.z);
         }
     };
 
